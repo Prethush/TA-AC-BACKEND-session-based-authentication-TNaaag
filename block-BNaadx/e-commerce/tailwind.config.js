0390a4521a24b1e54df49c3e63181f5f8732d2b0
Prethush <ktmirash49@.gmail.com>
2021-07-19 23:14:19 +0530

    added files

---
+++ block-BNaadx/e-commerce/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      height: {
+        custom: '88vh'
+      },
+      colors: {
+        custom: "#272727"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
